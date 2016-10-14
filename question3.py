ce5e67c1d459a9d8cd86686e13c5ad30d874cdb6
Terry Yeo Yee Shien <yyshien94@gmail.com>
2016-10-14 14:30:11 +0800

    Showing some result.

--- question3.py
+++ question3.py
@@ -18,7 +18,7 @@ df = pd.read_csv('/home/terry/Downloads/text_emotion.csv')
 #Tokenize the sentence to words and filter the sentence with "empty" sentiment and some short words (it is not useful)
 tweets = []
 for index, row in df[['content', 'sentiment']].iterrows():
-    line = str(row['content']).decode('utf-8','ignore')
+    line = str(row['content']).decode('utf-8','ignore')     #When tokenize the sentence, need to use .decode('utf-8', 'ignore') for avoiding from error. Further information: http://nedbatchelder.com/text/unipain.html
     for e in nltk.word_tokenize(line):
         if len(e) >= 3 and str(row['sentiment']) != 'empty':
             tweets.append((e.lower(), str(row['sentiment'])))
@@ -72,3 +72,49 @@ print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
 print 'accuracy:', nltk.classify.util.accuracy(classifier2, testfeats)
 print 'accuracy:', nltk.classify.util.accuracy(classif, testfeats)
 print 'accuracy:', nltk.classify.util.accuracy(classif2, testfeats)
+
+
+"""
+>>> df.info()
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 40000 entries, 0 to 39999
+Data columns (total 4 columns):
+tweet_id     40000 non-null int64
+sentiment    40000 non-null object
+author       40000 non-null object
+content      40000 non-null object
+dtypes: int64(1), object(3)
+memory usage: 1.2+ MB
+
+>>> classifier.labels()
+['love',
+ 'relief',
+ 'neutral',
+ 'anger',
+ 'sadness',
+ 'happiness',
+ 'surprise',
+ 'fun',
+ 'enthusiasm',
+ 'hate',
+ 'worry',
+ 'boredom']
+ 
+ #After running the above code, result will be like this:
+ 
+ Most Informative Features
+                 feature = u'love'          love : anger  =    163.2 : 1.0
+                 feature = u'happy'         love : boredo =    115.4 : 1.0
+                 feature = u'hate'          hate : boredo =    103.7 : 1.0
+                 feature = u'sad'         sadnes : fun    =     92.3 : 1.0
+                 feature = u'thanks'      happin : boredo =     79.2 : 1.0
+                 feature = u'mothers'       love : hate   =     78.4 : 1.0
+                 feature = u'http'        neutra : boredo =     72.2 : 1.0
+                 feature = u'day'           love : anger  =     66.9 : 1.0
+                 feature = u'good'        happin : boredo =     64.5 : 1.0
+                 feature = u'fun'            fun : anger  =     61.6 : 1.0
+accuracy: 0.32713963964
+accuracy: 0.282094594595
+accuracy: 0.326576576577
+accuracy: 0.311373873874
+"""
